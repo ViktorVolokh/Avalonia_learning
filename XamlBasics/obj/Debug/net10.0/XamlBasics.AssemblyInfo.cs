b0dbf33e747f565ef52e95db95e59382913156a8
volokhviktor <volohviktor1@gmail.com>
2026-07-18 20:00:40 +0200

    feat: add main window layout with buttons and textblock

--- XamlBasics/obj/Debug/net10.0/XamlBasics.AssemblyInfo.cs
+++ XamlBasics/obj/Debug/net10.0/XamlBasics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XamlBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afc12c79dc2a88917ae7bfa580e4d86834633b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("XamlBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XamlBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
